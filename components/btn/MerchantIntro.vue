d103b554a779fae3bb84ad031b811ede05b1ac96
tree6bi <155433024+tree6bi@users.noreply.github.com>
2024-11-18 11:51:29 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ components/btn/MerchantIntro.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps(['intro'])
+const intro = props.intro
+
+onMounted(() => {
+  if (intro) {
+    merchantNotice.showModal()
+  }
+});
+</script>
+
+<template>
+  <button onclick="merchantNotice.showModal()" class="btn bg-blue-700 hover:bg-blue-600 btn-sm rounded-md">
+    <span class="text-white font-bold">{{ $t('Notice') }}</span>
+  </button>
+  <dialog ref="merchantNotice" id="merchantNotice" class="modal">
+    <div class="modal-box">
+      <h3 class="text-lg font-bold">{{ $t('Notice') }}</h3>
+      <p class="py-4 whitespace-pre-wrap leading-relaxed">{{ intro }}</p>
+    </div>
+    <form method="dialog" class="modal-backdrop">
+      <button>close</button>
+    </form>
+  </dialog>
+</template>
